7e51889322cc6b3c772c50726712750a36d250cb
turbocsp <alesilva.mga@gmail.com>
2025-10-05 00:43:11 -0300

    Alterações para Vercel 6

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { supabase } from "../supabaseClient";
 import { useAuth } from "../context/AuthContext";
